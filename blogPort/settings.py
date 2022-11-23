cde5e645b1e0314cbd93e9dc87b868794af0e95e
Akolade <emmakolade@gmail.com>
2022-11-23 06:55:30 +0100

    secret key

--- blogPort/settings.py
+++ blogPort/settings.py
@@ -24,7 +24,7 @@ load_dotenv(os.path.join(BASE_DIR, ".env"))
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '&vew4rs#1f7@1qx2x*)(e-%t67y8q4i1a*4$93719!+92m(e4q'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -97,6 +97,17 @@ DATABASES = {
         'PORT': '6192',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': config('NAME'),
+#         'USER': config('USER'),
+#         'PASSWORD': config('PASSWORD'),
+#         'HOST': config('HOST'),
+#         'PORT': config('PORT'),
+#     }
+# }
 
 
 # Password validation
