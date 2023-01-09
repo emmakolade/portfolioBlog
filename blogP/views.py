80d60cf169016d8cf762df7fc0441633f8493f1f
Akolade <emmakolade@gmail.com>
2023-01-09 20:00:55 +0100

    ordering fixed

--- blogP/views.py
+++ blogP/views.py
@@ -8,6 +8,7 @@ from django.db.models import Q
 class Portpage(ListView):
     model = Project
     template_name = 'store/index.html'
+    ordering = ['-id']
 
     # def get_queryset(self):
     #     # original qs
